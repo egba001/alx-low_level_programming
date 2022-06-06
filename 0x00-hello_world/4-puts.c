758f12e0b208b441cee738ac2bc406e329b157c7
Nnaemeka <emekason128@gmail.com>
2022-06-06 04:55:32 -0700

    added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
-
-int main(void)
-
-{
+/**
+ *
+ * * main - program prints with function puts
+ *
+ * *
+ *
+ * *Return: Always 0
+ *
+ * */
+int main(void) {
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
