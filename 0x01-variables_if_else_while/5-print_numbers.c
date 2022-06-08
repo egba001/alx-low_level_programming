47bc6e1f8b898391261209128cc01328cdedc817
Nnaemeka <emekason128@gmail.com>
2022-06-08 15:06:57 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints single digits of base ten
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i =0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
