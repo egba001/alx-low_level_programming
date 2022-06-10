96d02c669ee0d2151473af05ccbc44eeca51e464
Nnaemeka <emekason128@gmail.com>
2022-06-10 16:58:36 -0700

    ok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_tables - prints time table
+ * print_times_table - prints time table
  * @n: times table to use
  * Return:void
  */
@@ -17,7 +17,7 @@ void print_times_table(int n)
 		for (b = 0; b <= n; b++)
 		{
 			rep = a * b;
-			if (b == 0 )
+			if (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
 			{
