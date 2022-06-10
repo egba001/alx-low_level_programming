276ff19115bf0d946bf72efb6ce7c6c8c298104a
Nnaemeka <emekason128@gmail.com>
2022-06-10 15:45:40 -0700

    ok

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - function that returns true for lower case number
+ *
+ * @c: char type letter
+ *
+ * Return: return 0 on success and 1 on fail
+ */
+
+int slower(int c)
+{
+	int i = islower(c);
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
