c04191ecb06fbef24dd9b2f818033fe2d0d15036
Nnaemeka <emekason128@gmail.com>
2022-06-14 14:33:42 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character
+ * *@str: pointer to char
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			printf("%c", str[i]);
+		}
+		i++;
+	}
+	printf('\n');
+}
