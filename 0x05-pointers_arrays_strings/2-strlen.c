01e8d743825250d5e68b26005c309d01882efe06
Nnaemeka <emekason128@gmail.com>
2022-06-14 14:13:16 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return length of string
+ * *@s: pointer to char
+ *
+ * Return: retur
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
