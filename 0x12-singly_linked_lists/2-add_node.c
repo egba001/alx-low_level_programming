856bce84564a24c738baeeb662a8acfeaa8d6f82
Egba Nnaemeka <emekason128@gmail.com>
2022-07-19 23:32:54 -0700

    removed whitespaces

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t nchar;
-
+	
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
+	
 	new->str = strdup(str);
-
+	
 	for (nchar = 0; str[nchar]; nchar++)
 		;
 	
