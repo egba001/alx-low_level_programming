789c577b6bd90ecdf46555c63448fe94450c8df2
Egba Nnaemeka <emekason128@gmail.com>
2022-07-20 02:57:11 -0700

    correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -31,41 +31,39 @@ void free_listp(listp_t **head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t print_listint_safe(const listint_t *head)
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
 	{
-		size_t nnodes = 0;
-		listp_t *hptr, *new, *add;
+		new = malloc(sizeof(listp_t));
 
-		hptr = NULL;
-		while (head != NULL)
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			new = malloc(sizeof(listp_t));
-
-			if (new == NULL)
-				exit(98);
-
-			new->p = (void *)head;
-			new->next = hptr;
-			hptr = new;
-
-			add = hptr;
-
-			while (add->next != NULL)
+			add = add->next;
+			if (head == add->p)
 			{
-				add = add->next;
-				if (head == add->p)
-				{
-					printf("-> [%p] %d\n", (void *)head, head->n);
-					free_listp(&hptr);
-					return (nnodes);
-				}
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
 			}
-
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-			nnodes++;
 		}
 
-		free_listp(&hptr);
-		return (nnodes);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
 	}
+
+	free_listp(&hptr);
+	return (nnodes);
+}
